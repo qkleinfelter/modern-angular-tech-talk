ce4551d3b6bd7ec688bd84f2abbfde54b4070742
Quinn Kleinfelter <quinn.kleinfelter@rovisys.com>
2024-08-05 12:35:24 -0400

    actually display the dog

--- src/app/pages/deferrable-views/components/api-request/api-request.component.ts
+++ src/app/pages/deferrable-views/components/api-request/api-request.component.ts
@@ -9,11 +9,14 @@ import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
   styleUrl: './api-request.component.scss'
 })
 export class ApiRequestComponent implements AfterViewInit {
+  imgSrc = '';
+
   constructor(private _httpClient: HttpClient) {}
 
   ngAfterViewInit(): void {
-    this._httpClient.get('https://dog.ceo/api/breeds/image/random').subscribe((data) => {
+    this._httpClient.get('https://dog.ceo/api/breeds/image/random').subscribe((data: any) => {
       console.log(data)
+      this.imgSrc = data.message;
     })
   }
 }
