e0b1b79d79a8cdf93ca191b9498a56be2f247f1e
Quinn Kleinfelter <quinn.kleinfelter@rovisys.com>
2024-08-05 11:08:59 -0400

    Standalone

--- src/app/pages/standalone/standalone.component.ts
+++ src/app/pages/standalone/standalone.component.ts
@@ -1,9 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-standalone',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, NgClass],
   templateUrl: './standalone.component.html',
   styleUrl: './standalone.component.scss'
 })
